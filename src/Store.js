2acc8d15fe08750b96be90ee62fc57a96fd1b74c
dihanmalik <abdulmaliknahid@gmail.com>
2020-05-25 00:33:43 +0800

    chore: apply requests handler reducer and middleware

--- src/Store.js
+++ src/Store.js
@@ -2,9 +2,11 @@ import { createStore } from 'redux'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
-import middlware from './Middleware'
+import createMiddleware from './Middleware'
+import createRequestsHandler from '@core/apiRequest'
+import { abortRequests } from '@redux-requests/core'
 
-import reducers from './Reducer'
+import createReducer from './Reducer'
 
 const persistConfig = {
   key: 'root',
@@ -13,10 +15,17 @@ const persistConfig = {
 }
 
 const storeCreator = () => {
+  const { requestsReducer, requestsMiddleware } = createRequestsHandler()
+  const middleware = createMiddleware(requestsMiddleware)
+  const reducers = createReducer({ requestsReducer })
 
-  const store = createStore(persistReducer(persistConfig, reducers), middlware)
+  const store = createStore(persistReducer(persistConfig, reducers), middleware)
 
   const persistor = persistStore(store)
+
+  // window.onbeforeunload = () => {
+  //   store.dispatch(abortRequests())
+  // }
 
   return {
     store,
